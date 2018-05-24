1409cc4bc3ea1b642eeeea626be277b766509e1d
edsilv <e.silverton@gmail.com>
2018-05-24 17:17:03 +0100

    allow undefined manifest

--- src/components/iiif-gallery/iiif-gallery.tsx
+++ src/components/iiif-gallery/iiif-gallery.tsx
@@ -26,27 +26,32 @@ export class IIIFGallery {
 
 	private _reset(): void {
 
-		manifesto.loadManifest(this.manifest).then((data) => {
+		if (this.manifest) {
+
+			manifesto.loadManifest(this.manifest).then((data) => {
 			
-			const manifest: Manifesto.IManifestResource = manifesto.create(data);
-
-			// if it's a collection, list either the child collections or child manifests.
-			if ((manifest as Manifesto.IIIIFResource).isCollection()) {
-				this.items = (manifest as Manifesto.ICollection).items;
-			} else {
-				// if it's a manifest, list the child canvases.
-				const sequences: Manifesto.ISequence[] = (manifest as Manifesto.IManifest).getSequences();
-
-				if (sequences.length) {
-					const sequence: Manifesto.ISequence = sequences[0];
-					this.items = sequence.getCanvases();
+				const manifest: Manifesto.IManifestResource = manifesto.create(data);
+	
+				// if it's a collection, list either the child collections or child manifests.
+				if ((manifest as Manifesto.IIIIFResource).isCollection()) {
+					this.items = (manifest as Manifesto.ICollection).items;
+				} else {
+					// if it's a manifest, list the child canvases.
+					const sequences: Manifesto.ISequence[] = (manifest as Manifesto.IManifest).getSequences();
+	
+					if (sequences.length) {
+						const sequence: Manifesto.ISequence = sequences[0];
+						this.items = sequence.getCanvases();
+					}
 				}
-			}
+	
+			}).catch(function(e) {
+				console.error(e);
+				console.error('failed to load manifest');
+			});
+			
+		}
 
-		}).catch(function(e) {
-			console.error(e);
-			console.error('failed to load manifest');
-		});
 	}
 
 	@Method()
